6b7bb8d57434a1ef032fce344528a2604a2ff746
Antony Chavez <ivanchr2199@outlook.com>
2021-05-06 10:12:58 -0500

    FIX: in main file grid -> pack

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Frame
+from tkinter import Tk, Button, Frame, LEFT
 from tkinter.ttk import Combobox
 from Modules.API_parameters_values import cryptocurrencies, time_series
 
@@ -11,9 +11,7 @@ def plot():
     #------------- indice del elemento seleccionado
     print(crypto_list.current())
     # the figure that will contain the plot
-    fig = Figure(figsize = (5, 5),
-                    dpi = 100)
-    # list of squares
+    fig = Figure(figsize = (5, 5), dpi = 100)
     y = [i**2 for i in range(101)]
     # adding the subplot
     plot1 = fig.add_subplot(111)
@@ -21,18 +19,16 @@ def plot():
     plot1.plot(y)
     # creating the Tkinter canvas
     # containing the Matplotlib figure
-    canvas = FigureCanvasTkAgg(fig,
-                                master = plot_frame)  
+    canvas = FigureCanvasTkAgg(fig, master = plot_frame)  
     canvas.draw()
     # placing the canvas on the Tkinter plot_frame
-    canvas.get_tk_widget().grid(row = 0, column = 1)
+    canvas.get_tk_widget().pack()
     # creating the Matplotlib toolbar
-    toolbar = NavigationToolbar2Tk(canvas,
-                                    plot_frame)
+    toolbar = NavigationToolbar2Tk(canvas, plot_frame)
     toolbar.update()
 
     # placing the toolbar on the Tkinter window
-    canvas.get_tk_widget().grid(row = 1, column = 1)
+    canvas.get_tk_widget().pack()
 
 path_curr = "./assets/digital_currency_list.csv"
 path_ph   = "./assets/physical_currency_list.csv"
@@ -50,23 +46,23 @@ window.title('Plotting in Tkinter')
 window.geometry("700x500")
 
 config_frame = Frame(window)
-config_frame.grid(row = 0, column = 0)
+config_frame.pack(side = LEFT)
 
 #list that displays cryptocurrencies
 crypto_list = Combobox(config_frame, text = "Cryptocurrencies")
-crypto_list.grid(row = 0, column = 0)
+crypto_list.pack()
 crypto_list["values"] = name_curr
 crypto_list.set("Bitcoin")
 
 #list price in physical currency
 Physical_currency = Combobox(config_frame, text = "currencies")
-Physical_currency.grid(row = 1, column = 0)
+Physical_currency.pack()
 Physical_currency["values"] = name_ph
 Physical_currency.set("Peruvian Nuevo Sol")
 
 #time intervals
 time_intervals = Combobox(config_frame, text = "time")
-time_intervals.grid(row = 2, column = 0)
+time_intervals.pack()
 time_intervals["values"] = list(time_series.keys())
 time_intervals.set("Intraday")
 
@@ -77,8 +73,8 @@ plot_button = Button(master = config_frame,
                     width = 10,
                     text = "Plot")
 
-plot_button.grid(row = 3, column = 0)
+plot_button.pack()
 
 plot_frame = Frame(window)
-plot_frame.grid(row = 0, column = 1)
+plot_frame.pack()
 window.mainloop()
