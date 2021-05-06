4a53f22ba470684fa96b54f3650f9461df55a43d
Antony Chavez <ivanchr2199@outlook.com>
2021-05-05 19:19:25 -0500

    ADD: price in physical money

--- Modules/API_parameters_values.py
+++ Modules/API_parameters_values.py
@@ -4,10 +4,8 @@ time_series = {"Intraday": "CRYPTO_INTRADAY",
                 "Weekly" : "DIGITAL_CURRENCY_WEEKLY",
                 "Monthly": "DIGITAL_CURRENCY_MONTHLY"
                 }
+#This function returns the code and names of the cryptocurrencies and their 
+#price in physical currency
 def cryptocurrencies(path: str) -> [list, list]:
     df = read_csv(path)
     return list(df["currency name"]),list(df["currency name"])
-
-
-
-
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import Tk, Button, Frame
 from tkinter.ttk import Combobox
-from Modules import API_parameters_values as param
+from Modules.API_parameters_values import cryptocurrencies
+
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
-path: str = "./assets/digital_currency_list.csv"
+
 def plot():
+    #------------- indice del elemento seleccionado
+    print(crypto_list.current())
     # the figure that will contain the plot
     fig = Figure(figsize = (5, 5),
                     dpi = 100)
@@ -30,9 +33,13 @@ def plot():
     # placing the toolbar on the Tkinter window
     canvas.get_tk_widget().grid(row = 1, column = 1)
 
+path_curr = "./assets/digital_currency_list.csv"
+path_ph   = "./assets/physical_currency_list.csv"
+name_curr, code_curr = cryptocurrencies(path_curr)
+name_ph, code_ph     = cryptocurrencies(path_ph)
+
+#---------------------------------------------------------
 # the main Tkinter window
-path = "./assets/digital_currency_list.csv"
-name, code = param.cryptocurrencies(path)
 window = Tk()
 
 # setting the title 
@@ -45,10 +52,17 @@ config_frame = Frame(window)
 config_frame.grid(row = 0, column = 0)
 
 #list that displays cryptocurrencies
-crypto_list = Combobox(config_frame, text = "Cryptocurrenci")
+crypto_list = Combobox(config_frame, text = "Cryptocurrencies")
 crypto_list.grid(row = 0, column = 0)
-crypto_list["values"] = name
+crypto_list["values"] = name_curr
+crypto_list.set("Bitcoin")
 
+#list price in physical currency
+Physical_currency = Combobox(config_frame, text = "currencies")
+Physical_currency.grid(row = 1, column = 0)
+Physical_currency["values"] = name_ph
+Physical_currency.set("Peruvian Nuevo Sol")
+#Physical_currency.set("Bitcoin")
 
 # button that displays the plot
 plot_button = Button(master = config_frame, 
@@ -57,7 +71,7 @@ plot_button = Button(master = config_frame,
                     width = 10,
                     text = "Plot")
 
-plot_button.grid(row = 1, column = 0)
+plot_button.grid(row = 2, column = 0)
 
 plot_frame = Frame(window)
 plot_frame.grid(row = 0, column = 1)
