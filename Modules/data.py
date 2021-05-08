71353d0ef476b7c5f391dcc3eee276e14cc36e4c
Antony Chavez <ivanchr2199@outlook.com>
2021-05-07 19:46:02 -0500

    ADD: function filter_data() implemented

--- Modules/data.py
+++ Modules/data.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_info(url, time_interval, crypto, physical_c, key):
+def get_info(url, time_interval, crypto, physical_c, key) -> dict:
     args = {
             "function": time_interval,
             "symbol"  : crypto,
@@ -8,11 +8,23 @@ def get_info(url, time_interval, crypto, physical_c, key):
             "apikey"  : key
             }
     response = requests.get(url, params = args)
-
     if response.status_code == 200:
         return response.json()
 
     print("request error")
 
-def filter_data():
-    pass
+def filter_data(data: dict, interval: str, curr:str) -> [list, list]:
+    t_series = f"Time Series (Digital Currency {interval})"
+    op       = f"4a. close ({curr})" #work with the closing price
+    date = list(data[t_series].keys())
+    date.reverse()
+    price = [float(data[t_series][d][op]) for d in date]
+
+    return date, price
+
+
+
+
+
+    
+
